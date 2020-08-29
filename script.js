cf5aed0b44ae1e48112a7b82d46252a1378c8398
Jordan Naei <jordannaei.engineer@gmail.com>
2020-08-29 15:12:10 -0400

    finalizesd some final touches by adding comments to explain the code

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// Generat Password funtion
 function generatePassword() {
+  // Introducing my local variable
   var numberOfChar;
   var includeLower;
   var includeUpper;
@@ -21,83 +24,80 @@ function generatePassword() {
   passwordArray = [];
   superarray = [];
 
+  // Number of charaters prompt and validation using console log for debugging purposes
   numberOfChar = prompt("Pick a desired number of characters between 8-128");
   console.log(numberOfChar);
-  if (numberOfChar >= 8 && numberOfChar <= 128) {
-    console.log("sakoo");
-  } else {
-    alert(
-      "'You entered an invalid number of characters, please refresh the page and provide a number between 8-128?"
-    );
-    return ("Click Generate Password Button Agian");
-  }
-  includeUpper = confirm(
-    "Do you need your password to have Uppercase Letters?"
-  );
-  console.log(includeUpper);
-  includeLower = confirm(
-    "Do you need your password to have Lowercase Letters?"
-  );
-  console.log(includeLower);
-  includeSymbols = confirm("Do you need your password to have Symbols?");
-  console.log(includeSymbols);
-  includeNumbers = confirm("Do you need your password to have Numbers?");
-  console.log(includeNumbers);
+    if (numberOfChar >= 8 && numberOfChar <= 128) {
+      console.log("sakoo");
+    } else {
+      alert(
+        "'You entered an invalid number of characters, please refresh the page and provide a number between 8-128?"
+      );
+      return "Click Generate Password Button Agian";
+    }
 
-  if (
-    includeLower === false &&
-    includeNumbers === false &&
-    includeSymbols === false &&
-    includeUpper === false
-  ) {
-    alert("At least one category need to be selected");
-    return;
-  } else {
-    console.log("sakoo");
-  }
-
+// geting the rest of the input elements
+    includeUpper = confirm("Do you need your password to have Uppercase Letters?");
+    console.log(includeUpper);
+    includeLower = confirm("Do you need your password to have Lowercase Letters?");
+    console.log(includeLower);
+    includeSymbols = confirm("Do you need your password to have Symbols?");
+    console.log(includeSymbols);
+    includeNumbers = confirm("Do you need your password to have Numbers?");
+    console.log(includeNumbers);
+// validate the negative scenario
+      if (
+        includeLower === false &&
+        includeNumbers === false &&
+        includeSymbols === false &&
+        includeUpper === false
+      ) {
+        alert("At least one category need to be selected");
+        return;
+      } else {
+        console.log("sakoo");
+      }
+// creating my superarray pool of elements
   function arrayCreation(start, finish) {
     const array = [];
     for (let q = start; q <= finish; q++) {
       array.push(q);
-    }
-    return array;
-  }
-  lowerCase = arrayCreation(97, 122);
-  console.log(lowerCase);
-  upperCase = arrayCreation(65, 90);
-  console.log(upperCase);
-  numbers = arrayCreation(48, 57);
-  console.log(numbers);
-  symbols = arrayCreation(33, 47)
-    .concat(arrayCreation(58, 64))
-    .concat(arrayCreation(91, 96))
-    .concat(arrayCreation(123, 126));
-  console.log(symbols);
+        }
+        return array;
+      }
+      lowerCase = arrayCreation(97, 122);
+      console.log(lowerCase);
+      upperCase = arrayCreation(65, 90);
+      console.log(upperCase);
+      numbers = arrayCreation(48, 57);
+      console.log(numbers);
+      symbols = arrayCreation(33, 47).concat(arrayCreation(58, 64)).concat(arrayCreation(91, 96)).concat(arrayCreation(123, 126));
+      console.log(symbols);
 
-  if (includeLower) {
-    superarray = superarray.concat(lowerCase);
-  } else {
-    console.log("lowerCasenotincluded");
-  }
-  if (includeUpper) {
-    superarray = superarray.concat(upperCase);
-  } else {
-    console.log("UpperCasenotincluded");
-  }
-  if (includeNumbers) {
-    superarray = superarray.concat(numbers);
-  } else {
-    console.log("Numbersnotincluded");
-  }
-  if (includeSymbols) {
-    superarray = superarray.concat(symbols);
-  } else {
-    console.log("Symbolsnotincluded");
-  }
+      if (includeLower) {
+        superarray = superarray.concat(lowerCase);
+      } else {
+        console.log("lowerCasenotincluded");
+      }
+      if (includeUpper) {
+        superarray = superarray.concat(upperCase);
+      } else {
+        console.log("UpperCasenotincluded");
+      }
+      if (includeNumbers) {
+        superarray = superarray.concat(numbers);
+      } else {
+        console.log("Numbersnotincluded");
+      }
+      if (includeSymbols) {
+        superarray = superarray.concat(symbols);
+      } else {
+        console.log("Symbolsnotincluded");
+      }
 
-  console.log("The final SuperArray" + superarray);
+      console.log("The final SuperArray" + superarray);
 
+  //shuffle the superarray for more randomness and also to try our best to avoid getting sequencec permutation
   for (let o = superarray.length - 1; o > 0; o--) {
     var r = Math.floor(Math.random() * o);
     var temp = superarray[o];
@@ -106,16 +106,16 @@ function generatePassword() {
   }
   console.log("The shuffled Superarray : " + superarray);
 
+  // looping the desired number of characters and generate a randon supperarray index number
   for (t = 1; t <= numberOfChar; t++) {
     var y = Math.floor(Math.random() * superarray.length);
     console.log(y);
     passwordArray.push(String.fromCharCode(superarray[y]));
   }
   console.log("This is the password array : " + passwordArray);
-
-  var secert = passwordArray.join("");
-  console.log(secert);
-  return secert;
+  
+// returning the password as a String not an array
+  return passwordArray.join("");
 }
 
 // Add event listener to generate button
